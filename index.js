29724c4b5ba4be4e0f9064a8ed57058628ee3188
johnkiguma <154498415+johnkiguma@users.noreply.github.com>
2023-12-27 06:13:55 +0300

    complete   assignment
    update.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Wrivar customerName = 'bob'
+  function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return customerName
+  }
+
+function setBestCustomer(){
+  bestCustomer ='not bob'
+  return bestCustomer
+}
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+
+}
+
+
+const leastFavoriteCustomer ='steve'
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'steve'
+  return leastFavoriteCustomer = 'probably steve'
+  function changeLeastFvoriteCustomer(){
+    leastFavoriteCustomer = 'not sarah'
+  }
+}te your solution in this file!
